0eb553f8c3ab9eb0feaea3d40d853504d9e2a5f4
rjrobben <rayipwm@gmail.com>
2021-10-17 03:32:29 +0000

    template

---
+++ build/CMakeFiles/tinyrenderer.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/tinyrenderer/main.cpp" "CMakeFiles/tinyrenderer.dir/main.cpp.o" "gcc" "CMakeFiles/tinyrenderer.dir/main.cpp.o.d"
+  "/workspace/tinyrenderer/tgaimage.cpp" "CMakeFiles/tinyrenderer.dir/tgaimage.cpp.o" "gcc" "CMakeFiles/tinyrenderer.dir/tgaimage.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
